38571eadbc2ba3dab2e65bd29db0dccf7f4170d2
Timothy Holt <tabholt@timworkmacbook.usilu.net>
2020-11-24 09:06:20 +0100

    Release Project7 to students.

---
+++ Projects/Project7/src/hpc-python/pde-miniapp-py/draw.py
@@ -0,0 +1,10 @@
+from pde_miniapp_py import viz
+
+# initial
+viz.draw("simulation", 0)
+viz.plt.savefig("simulation_initial.png")
+
+# final
+viz.draw("simulation", 100)
+viz.plt.savefig("simulation_T.png")
+
